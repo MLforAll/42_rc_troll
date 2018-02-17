422919eab79db2d37600b688f24e093c8e0ee70d
Kelian DUMARAIS <kdumarai@student.42.fr>
2018-02-17 21:48:55 +0100

    Better get_trolled.sh

--- get_trolled.sh
+++ get_trolled.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ssh kelian@51.255.193.81 "cat /var/www/mlforall/42/troll/pool/rc/trolled.txt"
+ssh kelian@51.255.193.81 "buff=\$(cat /var/www/mlforall/42/troll/pool/rc/trolled.txt); if [ -z \"\$buff\" ]; then echo \"Ya didn't catch anyone!\"; else echo \"\$buff\"; fi"
